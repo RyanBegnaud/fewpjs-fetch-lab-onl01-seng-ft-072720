17ed9f9e7088fa6024c70ba4b4b85a05140ab6d1
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 22:17:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks(){
-  fetch('https://anapioficeandfire.com/api/books/5')
+  fetch('https://anapioficeandfire.com/api/characters/1031')
     .then(resp => resp.json())
     .then(json => console.log(json));
 }
